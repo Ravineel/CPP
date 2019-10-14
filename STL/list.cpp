3c31bfb2f640c8516ce21d5a9523e30c9bad87a4
Ravineel <rajatsinghi89@gmail.com>
2019-10-14 19:51:55 +0530

    Update list.cpp

--- STL/list.cpp
+++ STL/list.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<list>
+#include<algorithm>
 
 using namespace std;
 
@@ -22,16 +23,39 @@ int main(){
     for(int i=0;i<n;i++)
     {   int x;
         cin>>x;
-        List1.insert(x);
+        List1.push_back(x);
     }
 
     //to find value in list, find[first,last,value) 
 
     int x ;
     cin>>x;
-    find(List1.begin(),List1.end(),x);
+    list<int> :: iterator it = find(List1.begin(),List1.end(),x);
+    //it will have the position to the element
 
-     
+    // to reverse a list
+
+    List1.reverse();
+
+    //to sort a list
+
+    List1.sort();
+
+    /*and many more functions
+    List1.splice();
+    List1.empty();
+    List1.erase();
+    List1.pop_back();
+    List1.pop_front();
+    List1.size();
+    List1.unique();
+    List1.swap();
+    List1.merge();
+    List1.resize();
+    List1.clear()
+    etc....
+    */
+
 
     return 0;
 }
