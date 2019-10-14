f933086880e59757a69d632816bd61ad06e49251
Ravineel <rajatsinghi89@gmail.com>
2019-10-14 19:25:02 +0530

    Update linklist.cpp

--- DATA STRUCTURE/linklist.cpp
+++ DATA STRUCTURE/linklist.cpp
@@ -9,7 +9,7 @@ typedef struct node{
     struct node *next;
 }node;
 
-node *head=NULL;
+node *head=NULL, *top=NULL;
 
 
 int main(){
@@ -22,22 +22,71 @@ int main(){
         cout<<"\nEnter 2 to delete an element ";
         cout<<"\nEnter 3 to search for an element ";
         cout<<"\nEnter 0 to exit ";
-        cout<<"Enter choice: ";
+        cout<<"\nEnter choice: ";
         cin>>ch;
-        time(100)
+     
         cout<<"\n-------------------------------------------------------------------------------------------------------------\n";
 
         switch(ch){
 
-            case 1: cout<<"\t\t\tInsertion of Elements\n";
-                    int a;
-                    cout<<"Enter value: "; cin>>a;
-                    node *p = (node*)malloc(sizeof(node));
-                    p->data = a;
-                    if(head == NULL)
-                        head = p;
-                    else
-                      
+            case 1: {
+						cout<<"\t\t\tInsertion of Elements\n";
+                    	int a;
+                    	cout<<"Enter value: "; cin>>a;
+                    	node *p = (node*)malloc(sizeof(node));
+                    	p->data = a;
+                    	p->next = NULL;
+                    	if(head == NULL)
+                        	head = top = p;
+                    	else{
+                         	top->next = p;
+                        	top = p; 
+                    	}
+                    	cout<<"Element inseterd\n-------------------------------------------------------------------------------------------------------------\n";
+                    	break;
+                }
+
+            case 2: {
+						cout<<"\t\t\tDeleting Element \n";
+                    	cout<<"Enter value to be deleted: ";
+                    	int a;
+                    	cin>>a; 
+                    
+                    
+                    	if(head->data==a){   
+							head=head->next;
+                        	cout<"Element deleted\n-------------------------------------------------------------------------------------------------------------\n";     
+                    
+                        	break;
+                    	}
+                    	node *temp = head;
+                    	node *t = NULL;
+                    	while(temp->next!=NULL){   
+							t=temp;
+                        	if(temp->data = a && temp!=head)
+                        		break;
+                        
+                        	temp=temp->next;
+                    
+                    	}   
+                    	if(temp->next==NULL) cout<"Element could not be found\n-------------------------------------------------------------------------------------------------------------\n";
+                    	else{
+                            t->next=temp->next;
+                       		cout<"Element deleted\n-------------------------------------------------------------------------------------------------------------\n";     
+                    		}
+                    	free(t);
+                    	free(temp);
+                    	break;
+					}
+
+            case 0:{
+			}
+
+            default:{
+				cout<<"Wrong choice Entered\n";
+				break;
+			
+			}          
 
         
         
