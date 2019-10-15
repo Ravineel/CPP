fa836662ab56ea5e8875718bb67a8518b5bc94b7
Ravineel <rajatsinghi89@gmail.com>
2019-10-15 18:39:43 +0530

    Update set.cpp

---
+++ STL/set.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<set>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+
+    /*
+    Set is a associative container, which stores
+    data in sorted order, by default its ascending order.
+    All data in set are unique. 
+   Syntax: set<data_type> set_name;
+    */
+
+   set<int> A;
+
+   //to get inport for n data in a set;
+
+   int n;
+   cin>>n;
+
+   for (int  i = 0; i <n; i++)
+   {
+        int x;
+        cin>>x;
+        A.insert(x);
+
+   }
+
+
+   
+
+
+    return 0;
+}
