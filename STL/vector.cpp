8464ebd5059c66c6c8d84c8e8a92f69cd8ed375a
Ravineel <rajatsinghi89@gmail.com>
2019-10-15 18:43:30 +0530

    Update vector.cpp

---
+++ STL/vector.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    vector<int>a;
+    a.push_back(10);
+    a.pop_back();
+    a.push_back(23);
+    int x = a.size();
+
+    
+    
+    return 0;
+}
