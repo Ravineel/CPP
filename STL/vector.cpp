973c141607d5b7f87cecf3beef3565dfe0bebae5
Ravineel <rajatsinghi89@gmail.com>
2019-12-26 09:05:30 +0800

    updated

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -15,7 +15,11 @@ int main(){
     a.push_back(23);
     a.push_back(23);
     a.push_back(23);
+    a.push_back(5);
+    a.push_back(3);
+    a.push_back(2);
     int x = a.size();
+<<<<<<< Updated upstream
     int y;
    
    cin>>y;
@@ -29,6 +33,11 @@ int main(){
     cout<<x<<endl;
     for (auto b : a)
     cout<<b<<" ";
+=======
+    cout<<x<<endl;
+
+    sort(a.begin(),a.end());
+>>>>>>> Stashed changes
     
     return 0;
 }
